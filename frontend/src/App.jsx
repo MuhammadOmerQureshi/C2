b47949fa96c218559ca70a976a2c29055e1719ec
MuhammadOmerQureshi <omer.qureshi0001@stud.hkr.se>
2025-04-30 19:46:42 +0200

    Employee Dahsboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+import React from 'react';
+import AppRoutes from './routes/AppRoutes';
+
+function App() {
+  return <AppRoutes />;
+}
+
+export default App;
+
+
+/*
 function App() {
   const [count, setCount] = useState(0)
 
@@ -33,3 +45,8 @@ function App() {
 }
 
 export default App
+
+*/
+
+
+
