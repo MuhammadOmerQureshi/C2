f122749025fb42d930a7a0f3d93643c30004964b
MuhammadOmerQureshi <omer.qureshi0001@stud.hkr.se>
2025-05-01 14:42:19 +0200

    import register pages in AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage      from '../pages/LoginPage';
 import EmployerDash   from '../pages/EmployerDashboard';
 import EmployeeDash   from '../pages/EmployeeDashboard';
+import RegisterPage   from '../pages/RegisterPage';
+
 
 export default function AppRoutes() {
   return (
