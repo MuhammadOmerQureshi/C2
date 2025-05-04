4122887a4ceb1d37124eb0d8f3c2f8ecc218ed76
MuhammadOmerQureshi <omer.qureshi0001@stud.hkr.se>
2025-05-05 01:49:35 +0200

    Users details

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  name:      { type: String, required: true },
-  email:     { type: String, required: true, unique: true },
-  password:  { type: String, required: true },
+  firstName:   { type: String, required: true },
+  lastName:    { type: String, required: true },
+  username:    { type: String, required: true, unique: true },
+  email:       { type: String, required: true, unique: true },
+  password:    { type: String, required: true },        // will store hashed
+  address:     { type: String },
+  contactNo:   { type: String },
+  employeeId:  { type: String, unique: true, sparse: true },
+
   role:      { 
     type: String, 
     enum: ['admin', 'employer', 'employee'], 
