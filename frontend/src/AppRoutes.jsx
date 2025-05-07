abe1781ea6f830c97352b66b1f162791470d5273
obadaHana <obadahana1997@gmail.com>
2025-05-07 12:31:30 +0200

    Update AppRoutes with new routes

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -1,3 +1,24 @@
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from '../pages/LoginPage';
+import EmployerDash from './pages/EmployerDashboard';
+import EmployeeDash from './pages/EmployeeDashboard';
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/employer" element={<EmployerDash />} />
+        <Route path="/employee-dashboard" element={<EmployeeDash />} /> {/* Fix here */}
+        {/* future routes */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+/*
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import EmployeeDashboard from './pages/EmployeeDashboard';
@@ -14,3 +35,4 @@ export default function AppRoutes() {
     </Router>
   );
 }
+  */
