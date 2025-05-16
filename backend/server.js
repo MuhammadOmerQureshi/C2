0580549b4f3b507b44e18e95941c28b34face414
EmperorOlashinbo <ib.olasunbo@gmail.com>
2025-05-16 14:19:28 +0200

    update package.json

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,6 @@ const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 
 
-
-
-
-
-
-
-
-
 // 2. Route imports
 const authRoutes        = require('./routes/authRoutes');
 const shiftRoutes      = require('./routes/shiftRoutes');
