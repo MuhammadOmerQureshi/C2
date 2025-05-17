9f877fd65b90af3d26a1bdc8a88b3f2ada7eddbc
EmperorOlashinbo <ib.olasunbo@gmail.com>
2025-05-17 22:12:05 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -137,10 +137,10 @@ mongoose.connect(process.env.MONGODB_URI)
   .catch((error) => console.error('MongoDB connection error:', error));
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail', // Use your email service (e.g., Gmail, Outlook, etc.)
+  service: 'brevo', 
   auth: {
-    user: process.env.EMAIL_USER, // Your email address
-    pass: process.env.EMAIL_PASS, // Your email password or app-specific password
+    user: process.env.EMAIL_USER, 
+    pass: process.env.EMAIL_PASS, 
   },
 });
 
